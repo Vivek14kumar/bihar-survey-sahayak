d3b721504a5b13b63edb95a745fce947560e441f
Vivek Kumar <vivek142000kumar@gamil.com>
2026-05-19 19:25:06 +0530

    ac im

--- src/lib/actions/aminActions.js
+++ src/lib/actions/aminActions.js
@@ -1,8 +1,8 @@
 // src/lib/actions/aminActions.js (Adjust path based on your project)
 import mongoose from "mongoose";
 // Import your database connection and Amin model here
-import { connectToDatabase } from "@/app/api/utils/dbConnect"; 
-import AminProfile from "@/aap/api/models/AminProfile"; 
+import { connectToDatabase } from "../mongodb"; 
+import AminProfile from "../../app/api/models/AminProfile"; 
 
 export async function getLiveAminProfilesForSitemap() {
   try {
